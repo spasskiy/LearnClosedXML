65e5be917a6e51616dacaea61e30b3323f8860b4
Spasskiy Pavel <spasskiy@mail.com>
2025-02-28 13:48:35 +0300

    Add read from file

--- LearnClosedXML/Program.cs
+++ LearnClosedXML/Program.cs
@@ -7,6 +7,10 @@ namespace LearnClosedXML
     {
         static void Main(string[] args)
         {
+            //------------------------------------------------------
+            //Задача №1 : Создать файл с нашими данными
+            //------------------------------------------------------
+
             // Создаем новую рабочую книгу
             var workbook = new XLWorkbook();
 
@@ -22,6 +26,28 @@ namespace LearnClosedXML
 
             //Сохраняем файл
             workbook.SaveAs("output.xlsx");
+
+            //---------------------------------------------
+            //Задача №2 : Считать данные из файла
+            //---------------------------------------------
+
+            // Открываем существующий файл
+            var workbook2 = new XLWorkbook("output.xlsx");
+            //Получаем первый лист
+            var worksheet2 = workbook2.Worksheet(1);
+
+            //Считываем нужные нам значения
+            string name = worksheet2.Cell(2, 1).Value.ToString();
+
+            //Если уверены в данных, то можно конечно сразу считать значение из ячейки, но через Try это более безопасный вариант
+            int age = 0;
+            if (worksheet2.Cell(2, 2).TryGetValue<int>(out int ageValue))
+                age = ageValue;
+            else
+                throw new Exception("Wrong data in Cell");
+            //int age = worksheet2.Cell(2, 2).GetValue<int>();
+
+            Console.WriteLine($"Name : {name}, Age : {age}");
         }
     }
 }
