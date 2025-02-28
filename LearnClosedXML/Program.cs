b6acb2c32642fa5b13a2b554e398fd80a9cf5c1f
Spasskiy Pavel <spasskiy@mail.com>
2025-02-28 13:18:38 +0300

    Create xlsx file done

--- LearnClosedXML/Program.cs
+++ LearnClosedXML/Program.cs
@@ -1,4 +1,5 @@
 ﻿using ClosedXML;
+using ClosedXML.Excel;
 
 namespace LearnClosedXML
 {
@@ -6,7 +7,21 @@ namespace LearnClosedXML
     {
         static void Main(string[] args)
         {
-            
+            // Создаем новую рабочую книгу
+            var workbook = new XLWorkbook();
+
+            // Добавляем новый лист
+            var worksheet = workbook.Worksheets.Add("First Sheet");
+
+            // Заполняем ячейки данными
+            worksheet.Cell(1, 1).Value = "Name";
+            worksheet.Cell(1, 2).Value = "Age";
+
+            worksheet.Cell(2, 1).Value = "Bill";
+            worksheet.Cell(2, 2).Value = 54;
+
+            //Сохраняем файл
+            workbook.SaveAs("output.xlsx");
         }
     }
 }
